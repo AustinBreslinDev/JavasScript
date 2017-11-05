ea21d1f9c12f7455af296605b1ed37756f58b634
BuckyAustin <buckyaustin@gmail.com>
2017-11-05 22:42:18 +0000

    Reduce codebase  gulpfile

--- ElectronDesktop/gulpfile.js
+++ ElectronDesktop/gulpfile.js
@@ -7,12 +7,30 @@ const util = require ("gulp-util");
 const htmlmin = require ("gulp-htmlmin");
 const cleaner = require ("gulp-clean");
 const { exec } = require('child_process');
-// const mocha = require("gulp-mocha");
 const istanbulReport = require("gulp-istanbul-report");
 const mochaPhantom = require("gulp-mocha-phantomjs");
 
+function errorHandle (cb, error) {
+    if (error) {
+        util.log("Error "+ error);
+        cb(error);
+    } else {
+        cb();
+    }
+}
 
-gulp.task("tslint", (cb) => {
+gulp.task("clean", (cb) => {
+    pump([
+        gulp.src("dist/**"),
+        cleaner({
+            read: false
+        })
+    ],(error) => {
+        errorHandle(cb, error);
+    });
+});
+
+gulp.task("typescript", ["clean"], (cb) => {
     pump([
         gulp.src("lib/*.ts"),
         tslint({
@@ -20,74 +38,21 @@ gulp.task("tslint", (cb) => {
             configuration: "tslint.json"
         }),
         tslint.report(),
-    ],(error) => {
-        if (error) {
-            util.log("Error "+ error);
-            cb(error);
-        } else {
-            util.log("Finished TSLint, no errors.");
-            cb();
-        }
-    });
-});
-
-gulp.task("clean", (cb) => {
-    pump([
-        gulp.src("dist/**/*.*"),
-        cleaner({
-            read: false
-        })
-    ],(error) => {
-        if (error) {
-            util.log("Error "+ error);
-            cb(error);
-        } else {
-            util.log("Finished deleting old files.");
-            cb();
-        }
-    });
-});
-
-gulp.task("tsc",["tslint", "clean"], (cb) => {
-    pump([
         gulp.src("lib/**/*.ts"),
         tsc(),
         uglifyjs(),
         gulp.dest("dist/")
     ],(error) => {
-        if (error) {
-            util.log("Error "+ error);
-            cb(error);
-        } else {
-            util.log("Finished TypeScript compiling.");
-            cb();
-        }
+        errorHandle(cb, error);
     });
 });
 
-gulp.task("staticFiles", ["tsc"], (cb) => {
+gulp.task("staticFiles", ["clean"], (cb) => {
     exec(`mkdir ${__dirname}/dist/views`);
     exec(`mkdir ${__dirname}/dist/styles`);
     pump([
-        gulp.src("lib/views/**"), 
-        gulp.dest("dist/views"),
-        gulp.src("lib/styles/**"),
-        gulp.dest("dist/styles")
-    ],(error) => {
-        if (error) {
-            util.log("Error "+ error);
-            cb(error);
-        } else {
-            util.log("Finished Copying Static Files.");
-            cb();
-        }
-    });
-});
-
-gulp.task("minifyStatic", ["staticFiles"], (cb) => {
-    pump([
-        gulp.src("dist/views/**/*.html")
-        .pipe(htmlmin({
+        gulp.src("lib/views/**"),
+        htmlmin({
             caseSensitive: true,
             collapseWhitespace: true,
             html5: true,
@@ -95,35 +60,17 @@ gulp.task("minifyStatic", ["staticFiles"], (cb) => {
             removeEmptyAttributes: true,
             removeScriptTypeAttributes: true,
             useShortDoctype: true
-        }))
-        .pipe(gulp.dest("dist/views/")),
-        gulp.src("dist/views/**/*.css")
-        .pipe(
-            htmlmin()
-        )
-        .pipe(gulp.dest("dist/views/"))
+        }), 
+        gulp.dest("dist/views"),
+        gulp.src("lib/styles/**"),
+        htmlmin(),
+        gulp.dest("dist/styles")
     ],(error) => {
-        if (error) {
-            util.log("Error "+ error);
-            cb(error);
-        } else {
-            util.log("Finished Minifying Static Files.");
-            cb();
-        }
+        errorHandle(cb, error);
     });
 });
 
 gulp.task("test",(cb) => {
-    function errorHandle (error) {
-        if (error) {
-            util.log("Error "+ error);
-            cb(error);
-        } else {
-            util.log("Finished Minifying Static Files.");
-            cb();
-        }
-    }
-
     const coverageFile = `${__dirname}/coverage/coverage.json`;
     const mochaPhantomOpts = {
       phantomjs: {
@@ -134,34 +81,33 @@ gulp.task("test",(cb) => {
 
     exec(`tsc ${__dirname}/test/*.ts --allowJS --jsx preserve`, (error) => {
         if (error) {
-            errorHandle(error);
+            errorHandle(error, cb);
         } else {
             pump([
                 gulp.src(`${__dirname}/test/**/*.js`,{read:false}),
                 mochaPhantom(mochaPhantomOpts),
                 gulp.src(coverageFile),
-                istanbulReport(),
-                
+                istanbulReport()
             ],(error) => {
                 if (error) {
-                    errorHandle(error);
+                    errorHandle(cb, error);
                 } else {
-                    exec(`rm ${__dirname}/test/*.js`,errorHandle);
+                    exec(`rm ${__dirname}/test/*.js`,(error) => {
+                        errorHandle(cb, error);
+                    });
                 }
             });
         }
     });
 });
 
-gulp.task('default',["tslint", "tsc", "staticFiles", "minifyStatic"], (cb) => {
-    util.log("Finished Build");
+gulp.task('default',["clean", "typescript", "staticFiles"], (cb) => {
     exec(`${__dirname}/node_modules/electron/cli.js ${__dirname}/dist/main.js`,(error, stdout, stderr) => {
-        if (error) {
-            util.log("Error "+ error);
-            cb(error);
-        } else {
-            util.log("Finished Loading application.");
-            cb();
-        }
+        util.log(stdout);
+        util.log("");
+        util.log(stderr);
+        util.log("");
+        util.log("Finished Build");
+        errorHandle(cb, error);
     });
 });
